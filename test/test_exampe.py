11dd8467e03eeae652b2ed50cbb6193f7cb94764
Jake Mingolla <jakemingolla@gmail.com>
2025-08-04 14:54:45 -0400

    added ruff

--- test/test_exampe.py
+++ test/test_exampe.py
@@ -1,8 +1,12 @@
+from typing import Literal
+
 import pytest
 
+
 @pytest.fixture
-def example_fixture():
+def example_fixture() -> Literal[1]:
     return 1
 
-def test_example(example_fixture):
+
+def test_example(example_fixture: Literal[1]) -> None:
     assert example_fixture == 1
